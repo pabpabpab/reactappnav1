aab0e713d1c05362808fc1a4b73e0491547d7c2b
pabpabpab <urallove@yandex.ru>
2022-09-28 17:48:27 +0500

    lesson 4

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './App.sass';
+import theme from './components/theme/theme';
+import { ThemeProvider } from '@mui/material/styles';
+import {createContext} from 'react';
+import {Route, Routes} from 'react-router-dom';
+
 import Contacts from './components/Contacts';
 import Messages from './components/Messages';
+import NoChat from './components/NoChat';
 import Input from './components/Input';
-import {createContext} from 'react';
 
 import useContacts from './components/myHooks/useContacts';
 import useCorrespondent from './components/myHooks/useCorrespondent';
@@ -11,15 +16,6 @@ import useFilteredMessages from './components/myHooks/useFilteredMessages';
 import useGetLastId from './components/myHooks/useGetLastId';
 import useBotAnswer from './components/myHooks/useBotAnswer';
 
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-const theme = createTheme({
-    palette: {
-        primary: {
-            main: '#527712',
-        },
-    },
-});
-
 export const AppContext = createContext(null);
 
 function App() {
@@ -27,7 +23,7 @@ function App() {
     const {correspondentId, changeCorrespondentIdCB} = useCorrespondent(contacts);
 
     const {msgState, dispatch, sendMessageCB} = useMessages();
-    const {filteredMessages} = useFilteredMessages(correspondentId, msgState);
+    // const {filteredMessages} = useFilteredMessages(correspondentId, msgState);
 
     const {getLastIdCB} = useGetLastId(msgState);
     useBotAnswer(msgState, correspondentId, getLastIdCB, dispatch);
@@ -38,8 +34,12 @@ function App() {
                 <div className="messenger-app">
                     <Contacts
                         contacts={contacts}/>
-                    <Messages
-                        messages={filteredMessages}/>
+                    <Routes>
+                        <Route path="/" element={<NoChat />} />
+                        <Route path="/chat/:chatId"
+                            element={<Messages messages={msgState?.messages} />} />
+                        <Route path="*" element={<NoChat />} />
+                    </Routes>
                     <Input
                         correspondentId={correspondentId}
                         sendMessageCB={sendMessageCB}
@@ -52,3 +52,6 @@ function App() {
 
 export default App;
 
+/*
+<Messages messages={filteredMessages}/>
+ */
